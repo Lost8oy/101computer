1d0507eee09a7035233553426ebced7eb12a5806
Gianmarco Paffetti <gianmarcopaffetti@gmail.com>
2023-07-19 11:36:01 +0200

    tables

---
+++ database/migrations/2023_07_19_092726_create_cables_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cables', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('position_id');
+            $table->foreignId('relations_id');
+            $table->string('type');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cables');
+    }
+};
