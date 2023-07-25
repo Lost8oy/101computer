101fe63cce29b716119467b1617a1329bcdf05a3
Gianmarco Paffetti <gianmarcopaffetti@gmail.com>
2023-07-25 12:26:27 +0200

    roles and seeders

---
+++ database/seeders/CountriesTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class CountriesTableSeeder extends Seeder
+{
+
+    /**
+     * Auto generated seed file
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+
+        \DB::table('countries')->delete();
+        \DB::table('countries')->insert(array (
+            0=>
+            array (
+                'id'=> 1,
+                'name'=> 'United States',
+                'description'=> 'none'
+            )
+        ))
+        
+        
+    }
+}
